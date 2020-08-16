186eb220c6f1e8620365100383591d1c69ba10c4
Jordan Roberts <jdrobs19@gmail.com>
2020-08-16 13:59:12 -0600

    form input and refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,34 @@
-var buttonE1 = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(){
+var taskFormHandler = function(event){
+    event.preventDefault();
+
+    var taskNameInput = document.querySelector("input[name = 'task-name']").value;
+    var taskTypeInput = document.querySelector("select[name ='task-type']").value;
+
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    createTaskEl(taskDataObj);
+}
+
+var createTaskEl = function(taskDataObj){
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
+    
+    //create div to hold task info and add to list item
+    var taskInfoE1 = document.createElement("div");
+    //give a class name
+    taskInfoE1.className = "task-info";
+    //add HTML content to div
+    taskInfoE1.innerHTML = "<h3 class = 'task-name'>" + taskDataObj.name + "</h3><span class = 'task-type'>" + taskDataObj.type + "</span>";
+    
+    listItemE1.appendChild(taskInfoE1);
+    
     tasksToDoE1.appendChild(listItemE1);
 }
 
-buttonE1.addEventListener("click", createTaskHandler);
+formE1.addEventListener("submit", taskFormHandler);
